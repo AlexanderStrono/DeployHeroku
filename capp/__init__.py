84fd36b5975c176a2757fa17f00af321387be4d2
AlexanderStrono <126338894+AlexanderStrono@users.noreply.github.com>
2023-04-21 11:44:46 +0200

    Update __init__.py

--- capp/__init__.py
+++ capp/__init__.py
@@ -7,7 +7,7 @@ import os
 application = Flask(__name__)
 
 ### Code GitHub
-# application.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
+application.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
 # DBVAR=os.environ['DATABASE_URL']
 # DBVAR="postgresql://username:os.environ.get(‘DB_PASSWORD’)@host:port/database"
 # DBVAR="postgresql://username:password@host:port/database"
@@ -15,7 +15,6 @@ application = Flask(__name__)
 # application.config['SQLALCHEMY_BINDS'] ={'transport': DBVAR}
 
 #Code computer
-application.config['SECRET_KEY'] = '3oueqkfdfas8ruewqndr8ewrewrouewrere44554'
 DBVAR="postgresql://dfdaqmieokmyms:cfc39cd7d516e516efcc5f1c0de0e5c756d69b48bff340daf522ecde1998ce52@ec2-34-242-199-141.eu-west-1.compute.amazonaws.com:5432/d49ukvjdihvmfj"
 application.config['SQLALCHEMY_DATABASE_URI'] = DBVAR 
 application.config['SQLALCHEMY_BINDS'] ={'transport': DBVAR}
